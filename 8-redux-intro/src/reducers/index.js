65099845323b022a8afaff310a42dea02af977ca
Tian Qiu <tian.qiu142@gmail.com>
2020-10-21 09:28:16 -0700

    started react-redux unit

---
+++ 8-redux-intro/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '3:20' },
+    { title: 'All Star', duration: '3:14' },
+    { title: 'I Want It That Way', duration: '4:29' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});

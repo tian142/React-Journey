6c52bce454cb652131e90a7d98b8bf8cc9ca1f70
Tian Qiu <tian.qiu142@gmail.com>
2020-10-22 09:35:39 -0700

    completed basic redux tutorial

--- 8-redux-intro/src/components/SongList.js
+++ 8-redux-intro/src/components/SongList.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
-const SongList = () => {
-  return <div>SongList</div>;
+const SongList = (props) => {
+  return (
+    <div className="ui divided list">
+      {props.songs.map((song) => (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
